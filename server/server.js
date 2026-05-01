444fc0ee8a8b5a68a3be28d229ece17041ed15e9
tanu <tanuawasthi2005@gmail.com>
2026-05-01 23:16:24 +0530

    fix railway build and static path

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.get('/api/health', (req, res) => {
 });
 
 const __dirname = path.resolve();
-const clientBuildPath = path.join(__dirname, '../client/build');
+const clientBuildPath = path.join(__dirname, 'public');
 app.use(express.static(clientBuildPath));
 
 app.get('*', (req, res) => {
